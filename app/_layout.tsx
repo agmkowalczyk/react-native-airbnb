2e92cd7aebfb01df3574fecc7b32f9e517919daf
agmkowalczyk <agm.kowalczyk@gmail.com>
2024-01-06 01:00:16 +0100

    Add modals

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,8 @@
+import { Ionicons } from '@expo/vector-icons'
 import { useFonts } from 'expo-font'
-import { SplashScreen, Stack } from 'expo-router'
+import { SplashScreen, Stack, useRouter } from 'expo-router'
 import { useEffect } from 'react'
-import { useColorScheme } from 'react-native'
+import { Pressable } from 'react-native'
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -42,11 +43,44 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme()
+  const router = useRouter()
 
   return (
     <Stack>
       <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+      <Stack.Screen
+        name='(modals)/login'
+        options={{
+          title: 'Log in or sign up',
+          headerTitleStyle: {
+            fontFamily: 'mon-sb',
+          },
+          presentation: 'modal',
+          headerLeft: () => (
+            <Pressable onPress={() => router.back()}>
+              <Ionicons name='close-outline' size={28} />
+            </Pressable>
+          ),
+        }}
+      />
+      <Stack.Screen
+        name='listing/[id]'
+        options={{
+          headerTitle: '',
+        }}
+      />
+      <Stack.Screen
+        name='(modals)/booking'
+        options={{
+          presentation: 'transparentModal',
+          animation: 'fade',
+          headerLeft: () => (
+            <Pressable onPress={() => router.back()}>
+              <Ionicons name='close-outline' size={28} />
+            </Pressable>
+          ),
+        }}
+      />
     </Stack>
   )
 }
---
+++ app/listing/[id].tsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { useLocalSearchParams } from 'expo-router'
+
+const Page = () => {
+  const { id } = useLocalSearchParams<{ id: string }>()
+
+  return (
+    <View>
+      <Text>Page {id}</Text>
+    </View>
+  )
+}
+
+export default Page
