e42e75d10881a278270d309e224355e3b5317228
agmkowalczyk <agm.kowalczyk@gmail.com>
2024-01-06 04:13:15 +0100

    Add login ui

---
+++ app/(modals)/login.style.js
@@ -0,0 +1,38 @@
+import Colors from '@/constants/Colors'
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 26,
+  },
+  separatorView: {
+    flexDirection: 'row',
+    gap: 10,
+    alignItems: 'center',
+    marginVertical: 30,
+  },
+  separator: {
+    fontFamily: 'mon-sb',
+    color: Colors.grey,
+  },
+  btnOutline: {
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: Colors.grey,
+    height: 50,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    paddingHorizontal: 10,
+  },
+  btnOutlineText: {
+    color: '#000',
+    fontSize: 16,
+    fontFamily: 'mon-sb',
+  },
+})
+
+export default styles
--- app/(modals)/login.tsx
+++ app/(modals)/login.tsx
@@ -1,10 +1,67 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextInput, Pressable } from 'react-native'
 import React from 'react'
+import { useWarmUpBrowser } from '@/hooks/useWarmUpBrowser'
+
+import styles from './login.style'
+import defaultStyles from '@/constants/Styles'
+import { Ionicons } from '@expo/vector-icons'
+import Colors from '@/constants/Colors'
 
 const Page = () => {
+  useWarmUpBrowser()
+
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={styles.container}>
+      <TextInput
+        autoCapitalize='none'
+        placeholder='Email'
+        placeholderTextColor={Colors.grey}
+        style={[defaultStyles.inputField, { marginBottom: 30 }]}
+      />
+      <Pressable style={defaultStyles.btn}>
+        <Text style={defaultStyles.btnText}>Continue</Text>
+      </Pressable>
+
+      <View style={styles.separatorView}>
+        <View style={[{ flex: 1 }, defaultStyles.hairline]} />
+        <Text style={styles.separator}>or</Text>
+        <View style={[{ flex: 1 }, defaultStyles.hairline]} />
+      </View>
+
+      <View style={{ gap: 20 }}>
+        <Pressable style={styles.btnOutline}>
+          <Ionicons
+            name='call-outline'
+            size={24}
+            style={defaultStyles.btnIcon}
+          />
+          <Text style={styles.btnOutlineText}>Continue with Phone</Text>
+        </Pressable>
+        <Pressable style={styles.btnOutline}>
+          <Ionicons
+            name='md-logo-apple'
+            size={24}
+            style={defaultStyles.btnIcon}
+          />
+          <Text style={styles.btnOutlineText}>Continue with Apple</Text>
+        </Pressable>
+        <Pressable style={styles.btnOutline}>
+          <Ionicons
+            name='md-logo-google'
+            size={24}
+            style={defaultStyles.btnIcon}
+          />
+          <Text style={styles.btnOutlineText}>Continue with Google</Text>
+        </Pressable>
+        <Pressable style={styles.btnOutline}>
+          <Ionicons
+            name='md-logo-facebook'
+            size={24}
+            style={defaultStyles.btnIcon}
+          />
+          <Text style={styles.btnOutlineText}>Continue with Facebook</Text>
+        </Pressable>
+      </View>
     </View>
   )
 }
