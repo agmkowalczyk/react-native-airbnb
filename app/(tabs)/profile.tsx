378a11e17534e56b605339b6ed7bcf429bf2a032
agmkowalczyk <agm.kowalczyk@gmail.com>
2024-01-06 23:59:32 +0100

    Profile

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,20 +1,103 @@
-import { View, Text, Button } from 'react-native'
-import React from 'react'
-import { useAuth } from '@clerk/clerk-expo'
+import {
+  View,
+  Text,
+  Button,
+  SafeAreaView,
+  Pressable,
+  Image,
+} from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { useAuth, useUser } from '@clerk/clerk-expo'
 import { Link } from 'expo-router'
+import { Ionicons } from '@expo/vector-icons'
+
+import defaultStyles, { profilestyles as styles } from '@/constants/Styles'
+import Color from '@/constants/Colors'
+import { Fonts } from '@/types'
+import Colors from '@/constants/Colors'
 
 const Page = () => {
   const { signOut, isSignedIn } = useAuth()
+  const { user } = useUser()
+  const [firstName, setFirstName] = useState(user?.firstName)
+  const [lastName, setLastName] = useState(user?.lastName)
+  const [email, setEmail] = useState(user?.emailAddresses[0].emailAddress)
+  const [edit, setEdit] = useState(false)
+
+  useEffect(() => {
+    if (!user) return
+
+    setFirstName(user.firstName)
+    setLastName(user.lastName)
+    setEmail(user.emailAddresses[0].emailAddress)
+  }, [user])
+
+  const onSaveUser = async () => {
+    // try {
+    //   await user?.update({
+    //     firstName: firstName!,
+    //     lastName: lastName!,
+    //   })
+    // } catch (error) {
+    //   console.log(error)
+    // } finally {
+    setEdit(false)
+    // }
+  }
+
+  const onCaptureImage = async () => {}
 
   return (
-    <View>
-      <Button title='Log out' onPress={() => signOut()} />
+    <SafeAreaView style={defaultStyles.container}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.header}>Profile</Text>
+        <Ionicons name='notifications-outline' size={26} />
+      </View>
+
+      {user && (
+        <View style={styles.card}>
+          <Pressable onPress={onCaptureImage}>
+            <Image source={{ uri: user?.imageUrl }} style={styles.avatar} />
+          </Pressable>
+          <View style={{ flexDirection: 'row', gap: 6 }}>
+            {edit ? (
+              <View style={styles.editRow}>
+                <Pressable onPress={onSaveUser}>
+                  <Ionicons
+                    name='checkmark-outline'
+                    size={24}
+                    color={Colors.dark}
+                  />
+                </Pressable>
+              </View>
+            ) : (
+              <View style={styles.editRow}>
+                <Text style={{ fontFamily: Fonts.monB, fontSize: 22 }}>
+                  {firstName} {lastName}
+                </Text>
+                <Pressable onPress={() => setEdit(true)}>
+                  <Ionicons
+                    name='create-outline'
+                    size={24}
+                    color={Colors.dark}
+                  />
+                </Pressable>
+              </View>
+            )}
+          </View>
+          <Text>{email}</Text>
+          <Text>Since {user?.createdAt?.toLocaleDateString()}</Text>
+        </View>
+      )}
+      {isSignedIn && (
+        <Button title='Log out' onPress={() => signOut()} color={Color.dark} />
+      )}
       {!isSignedIn && (
-        <Link href='/(modals)/login'>
-          <Text>Log in</Text>
+        <Link href='/(modals)/login' asChild>
+          <Button title='Log in' color={Color.dark} />
         </Link>
       )}
-    </View>
+    </SafeAreaView>
   )
 }
 
--- constants/Styles.ts
+++ constants/Styles.ts
@@ -60,3 +60,47 @@ const defaultStyles = StyleSheet.create({
 })
 
 export default defaultStyles
+
+export const profilestyles = StyleSheet.create({
+  headerContainer: {
+    flexDirection: 'row',
+    padding: 24,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  header: {
+    fontFamily: Fonts.monB,
+    fontSize: 24,
+  },
+  card: {
+    backgroundColor: '#fff',
+    padding: 24,
+    borderRadius: 16,
+    marginHorizontal: 24,
+    marginTop: 24,
+    elevation: 2,
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    shadowOffset: {
+      width: 1,
+      height: 2,
+    },
+    alignItems: 'center',
+    gap: 14,
+    marginBottom: 24,
+  },
+  avatar: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    backgroundColor: Colors.grey,
+  },
+  editRow: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 8,
+  },
+})
